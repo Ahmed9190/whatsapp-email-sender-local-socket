37d23e29e45a26c9bd7f08086877c5a84f9baf4c
Ahmed Waleed <ahmed.waleed919@outlook.com>
2023-08-27 14:34:50 +0300

    Updated upload document button selector

--- src/features/whatsapp/whatsapp-selector.ts
+++ src/features/whatsapp/whatsapp-selector.ts
@@ -2,7 +2,7 @@ export default class Selector {
   static attachButton: string =
     "#main > footer > div:nth-child(1) > div > span:nth-child(2) > div > div:nth-child(1) > div > div > div";
   static uploadDocumentButton: string =
-    "#main > footer > div:nth-child(1) > div > span:nth-child(2) > div > div:nth-child(1) > div > div > span > div > ul > div > div:nth-child(4) > li > div > span";
+    "#main > footer > div:nth-child(1) > div > span:nth-child(2) > div > div:nth-child(1) > div > div > span > div > ul > div > div:nth-child(1) > li > div > span";
   static messageInput =
     "#main > footer > div:nth-child(1) > div > span:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(1) > p";
 }
